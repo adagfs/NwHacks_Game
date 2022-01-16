5b046179bbbd2bceeb51b1d5d182d7ec7e352e2c
Alice Kang <akang28@students.cs.ubc.ca>
2022-01-15 21:39:52 -0800

    Added object interaction

---
+++ Assets/Interactor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+	GameObject currentObj = null;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Interactable"))
+		{
+			currentObj = other.gameObject;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Interactable"))
+		{
+			if (other.gameObject == currentObj)
+			{
+				currentObj = null;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButtonDown("Interact") && currentObj)
+		{
+			// change character to have umbrella 
+			currentObj.SendMessage("DoInteraction");
+		}
+
+	}
+}
